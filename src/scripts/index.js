a9aff191245d7d7f8773b966729673ca711d7eb0
Marcelo Barsotti <mbarsott@gmail.com>
2019-03-14 22:07:10 -0400

    2.3 let and var for variable declarations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,19 @@
-console.log("Hello Webpack!!!");
+// const carId; // error, constants have to be initialized
+const carId = 100;
+// carId = 5; // error, constants are read only and cannot be reassigned;
+
+console.log(varId); // variables declared ahead with var will result in undefined
+var varId = 5;
+
+console.log(letId); // this should generate and error but results in undefined instead. Has it changed in ECMAScript?
+let letId = 10;
+
+if (true) {
+  var varScoped = 15;
+}
+console.log(varScoped); // variables defined with var have global scope
+
+if (true) {
+  let letScoped = 10; // scope is limited withing the block
+}
+// console.log(letScoped); // error, let defines variables with local scope
